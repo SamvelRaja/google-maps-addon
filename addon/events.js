59948e24ad50801ace73dc49be11e02e8ff9d043
Erik Rothoff Andersson <erik.rothoff@gmail.com>
2016-01-17 15:27:56 +0100

    Generalize the mouse-events list to support all events on a map instance

---
+++ addon/events.js
@@ -0,0 +1,29 @@
+/**
+@const events
+@type array
+A list of all possible events that can be called on a Maps object.
+  ref 'http://gmaps-samples-v3.googlecode.com/svn/trunk/map_events/map_events.html'
+**/
+const EVENTS = [
+  'click',
+  'dblclick',
+  'drag',
+  'dragend',
+  'dragstart',
+  'mousemove',
+  'mouseout',
+  'mouseover',
+  'rightclick',
+  'bounds_changed',
+  'center_changed',
+  'heading_changed',
+  'idle',
+  'maptypeid_changed',
+  'projection_changed',
+  'resize',
+  'tilesloaded',
+  'tilt_changed',
+  'zoom_changed'
+];
+
+export default EVENTS;
